31d509d21b31a530abf69591b2f50b190f144ef7
Clement Breau <clement.breau@etudiant.univ-lr.fr>
2026-03-05 14:42:52 +0100

    hello commit reader

--- cooldown.js
+++ cooldown.js
@@ -7,8 +7,7 @@ const cooldown = 22 * 60 * 1000;
 
 function setPresence() {
     rpc.setActivity({
-        details: "Still stuck with the fishes",
-        state: "",
+        details: "Still stuck with the fishesgi",
         endTimestamp: Date.now() + cooldown,
         instance: false
     });
